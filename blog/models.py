004ac2ff89596301cfbb47258838fb0f10dcdd06
John <j.susnik92@gmail.com>
2023-03-27 14:27:05 -0400

    start page 59 - class based views

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
 class Post(models.Model):
     
@@ -9,7 +14,8 @@ class Post(models.Model):
         PUBLISHED = ('PB', 'Published')
     
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
@@ -21,6 +27,9 @@ class Post(models.Model):
                               choices=Status.choices,
                               default=Status.DRAFT)
     
+    objects = models.Manager() # The default manager
+    published = PublishedManager() # Our custom manager.
+    
     class Meta:
         ordering = ['-publish']
         indexes = [
@@ -29,3 +38,10 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug])
+    
