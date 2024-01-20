96e356bab4ab499b0765dac254e7fa1b63a0a7f0
John <j.susnik92@gmail.com>
2024-01-20 13:37:04 -0500

    minor updates

--- blog/urls.py
+++ blog/urls.py
@@ -8,14 +8,8 @@ urlpatterns = [
     # post views
     path('', views.post_list, name='post_list'),
     #path('', views.PostListView.as_view(), name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/', 
-         views.post_detail, 
-         name='post_detail'),
-    path('<int:post_id>/share/', 
-         views.post_share, name='post_share'),
-    path('<int:post_id>/comment/',
-         views.post_comment, name='post_comment'),
-    path('tag/<slug:tag_slug>/',
-         views.post_list, name='post_list_by_tag'), # see page 97
-    path('feed/', LatestPostsFeed(), name='post_feed'), # see page 124
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
+    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
 ]
